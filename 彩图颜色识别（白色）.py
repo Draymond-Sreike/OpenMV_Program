15c6a6db560db63daa98cde2b2417620c4c47239
梁深南 <110004355+Draymond-Sreike@users.noreply.github.com>
2022-08-10 20:43:32 +0800

    Add files via upload

---
+++ 彩图颜色识别（白色）.py
@@ -0,0 +1,34 @@
+# Single Color RGB565 Blob Tracking Example
+#
+# This example shows off single color RGB565 tracking using the OpenMV Cam.
+
+import sensor, image, time, math
+
+threshold_index = 0 # 0 for red, 1 for green, 2 for blue
+
+# Color Tracking Thresholds (L Min, L Max, A Min, A Max, B Min, B Max)
+# The below thresholds track in general red/green/blue things. You may wish to tune them...
+thresholds = [((38, 93, -128, 127, -29, 2))] # generic_blue_thresholds
+
+sensor.reset()
+sensor.set_pixformat(sensor.RGB565)
+sensor.set_framesize(sensor.QVGA)
+sensor.skip_frames(time = 2000)
+sensor.set_auto_gain(False) # must be turned off for color tracking
+sensor.set_auto_whitebal(False) # must be turned off for color tracking
+clock = time.clock()
+
+# Only blobs that with more pixels than "pixel_threshold" and more area than "area_threshold" are
+# returned by "find_blobs" below. Change "pixels_threshold" and "area_threshold" if you change the
+# camera resolution. "merge=True" merges all overlapping blobs in the image.
+
+while(True):
+    clock.tick()
+    img = sensor.snapshot()
+    for blob in img.find_blobs([thresholds[threshold_index]], pixels_threshold=200, area_threshold=200, merge=True):
+        # These values depend on the blob not being circular - otherwise they will be shaky.
+        img.draw_rectangle(blob.rect())
+        img.draw_cross(blob.cx(), blob.cy())
+        # Note - the blob rotation is unique to 0-180 only.
+        print(blob.count())
+    print(clock.fps())
